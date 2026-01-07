b7d45f515521c6fdc164fcd177bae44f3589225d
ayushrayal19@gmail.com <ayushrayal19@gmail.com>
2026-01-07 17:37:01 +0530

    Basic UI With React (one page is done.).

---
+++ 01-basic-ui-with-react/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+    <h2>Horizon Courts</h2>
+      <a href="">About Us</a>
+      <a href="">Services</a>
+      <a href="">Contacts</a>
+      <a href="">Events</a>
+      <button>Book Now</button>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ 01-basic-ui-with-react/src/components/SectionOne.jsx
@@ -0,0 +1,14 @@
+const SectionOne = () => {
+  return (
+    <div className="Sectionone">
+      <div className="container">
+        <img className="containerimg" src="./src/assets/badminton.jpg" alt="" />
+        <h1 className="sectiononeh1">Unleash Your Inner Champion Today. All In One Place.</h1>
+        <p className="sectiononep">Join the ultimate experience -- where passion meets performance. and every swing brings you closer to victory.</p>
+        <button className="sectiononebtn">Start your own journey</button>
+      </div>
+    </div>
+  )
+}
+
+export default SectionOne
