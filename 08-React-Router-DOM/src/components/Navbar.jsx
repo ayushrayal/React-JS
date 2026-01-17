9b116cb865568c7ff620308f9ef3b135f617749b
ayushrayal19@gmail.com <ayushrayal19@gmail.com>
2026-01-17 23:36:05 +0530

    React Router DOM Ends.

--- 08-React-Router-DOM/src/components/Navbar.jsx
+++ 08-React-Router-DOM/src/components/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='h-30 w-full bg-fuchsia-950 border-b-2 border-amber-50 flex items-center justify-evenly'>
-      <Link className='text-5xl text-fuchsia-900 font-semibold uppercase' to="/">Home</Link>
-      <Link className='text-5xl text-fuchsia-900 font-semibold uppercase' to="/about">About</Link>
-      <Link className='text-5xl text-fuchsia-900 font-semibold uppercase' to="/contact">Contact</Link>
+      <NavLink className='text-5xl text-fuchsia-900 font-semibold uppercase' style={({ isActive})=>({
+        color: isActive?"purple":"white",
+        borderBottom:isActive?"3px purple solid":"none"
+      })} to="/">Home</NavLink>
+      <NavLink className='text-5xl text-fuchsia-900 font-semibold uppercase' style={({ isActive})=>({
+        color: isActive?"purple":"white",
+        borderBottom:isActive?"3px purple solid":"none"
+      })} to="/about">About</NavLink>
+      <NavLink className='text-5xl text-fuchsia-900 font-semibold uppercase' style={({ isActive})=>({
+        color: isActive?"purple":"white",
+        borderBottom:isActive?"3px purple solid":"none"
+      })} to="/contact">Contact</NavLink>
     </div>
   )
 }
